44cfdb9bef2736a4dca7bce397b71273c27d421b
Utsav Mukherjee <utsavmukherjee12@gmail.com>
2025-03-28 11:48:16 +0530

    final push

--- project.py
+++ project.py
@@ -16,36 +16,45 @@ def is_safe_state(processes, resources, allocation, max_demand, available):
     while len(safe_sequence) < len(processes):
         allocated = False
         for i in range(len(processes)):
-            if not finish[i] and all(need[i] <= work):
+            if not finish[i] and all(need[i, j] <= work[j] for j in range(len(resources))):
                 work += allocation[i]
                 finish[i] = True
                 safe_sequence.append(processes[i])
                 allocated = True
-                break
         if not allocated:
             return False, []
     return True, safe_sequence
 
-def detect_deadlock(processes, resources, allocation, max_demand):
+def detect_deadlock(processes, resources, allocation, max_demand, available):
     G = nx.DiGraph()
+    
+    # Add process and resource nodes
     for p in processes:
         G.add_node(p, color='blue')
     for r in resources:
         G.add_node(r, color='red')
 
+    allocation = np.array(allocation)
+    max_demand = np.array(max_demand)
+    available = np.array(available)
+    need = max_demand - allocation
+
+    # Add edges based on resource allocation and needs
     for i, process in enumerate(processes):
         for j, res in enumerate(resources):
-            if allocation[i][j] > 0:
-                G.add_edge(res, process)
-            if max_demand[i][j] > allocation[i][j]:
+            if need[i][j] > 0 and allocation[i][j] == 0 and available[j] == 0:
+                # Process needs a resource but it's not available (waiting edge)
                 G.add_edge(process, res)
+            if allocation[i][j] > 0:
+                # Process is holding a resource (holding edge)
+                G.add_edge(res, process)
 
+    # Detect cycles in the resource allocation graph
     try:
-        cycle = nx.find_cycle(G, orientation='original')
+        cycle = list(nx.find_cycle(G, orientation='original'))
         return True, cycle
     except nx.NetworkXNoCycle:
         return False, []
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -60,7 +69,7 @@ def simulate():
     available = data['available']
 
     safe, sequence = is_safe_state(processes, resources, allocation, max_demand, available)
-    deadlock, cycle = detect_deadlock(processes, resources, allocation, max_demand)
+    deadlock, cycle = detect_deadlock(processes, resources, allocation, max_demand, available)
 
     return jsonify({
         "safe_state": safe,
